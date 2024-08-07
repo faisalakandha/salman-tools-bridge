3e9e42739c28459e5d16f3f2530107c675153150
Faisal Akandha <57830958+faisalakandha@users.noreply.github.com>
2024-08-07 03:28:44 +0000

    Pricing Page Completed

--- frontend/src/app/pricing/page.tsx
+++ frontend/src/app/pricing/page.tsx
@@ -7,7 +7,7 @@ const PricingPage = () =>{
         <div>
             <h1 className="text-2xl py-8 font-semibold">Buy Subscriptions</h1>
 
-            <div className="grid grid-cols-4 gap-x-4">
+            <div className="flex flex-col md:flex-row items-center justify-center gap-y-6 gap-x-4">
 
                 <PricingTable />
                 <PricingTable />
